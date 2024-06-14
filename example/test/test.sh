1133582d681d161665dd68648fd849a6d305a1eb
NathanGuyard <nathan.arkanoxd12@gmail.com>
2024-06-14 14:23:35 -0700

    test: test sh pour lancer les deux

--- example/test/test.sh
+++ example/test/test.sh
@@ -12,8 +12,15 @@ export MSM_OUTPUT="/home/school/Desktop/my_alloc/example/report_file.log"
 # Définit LD_PRELOAD pour utiliser votre bibliothèque
 export LD_PRELOAD="/home/school/Desktop/my_alloc/example/libmy_secmalloc.so"
 
-# Exécute la commande passée en argument via gdb
-gdb --batch --ex "run" --ex "bt" --ex "quit" --args "$@"
+# Affiche les variables d'environnement pour le débogage
+echo "MSM_OUTPUT=$MSM_OUTPUT"
+echo "LD_PRELOAD=$LD_PRELOAD"
+
+# Exécute la commande passée en argument
+"$@"
+
+# Affiche un message de fin
+echo "Command executed: $@"
 
 # Annule la définition de LD_PRELOAD
 unset LD_PRELOAD
